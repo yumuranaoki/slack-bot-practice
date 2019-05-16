5ce095b875bb1250caffc7b1cdff7431e8814b31
yumuranaoki <naoki.yumura@gmail.com>
2019-05-17 00:17:57 +0900

    take args

--- mybot/scripts/test.js
+++ mybot/scripts/test.js
@@ -1,28 +1,24 @@
 const path = require('path');
+const spawn = require('child_process').spawn;
+const scriptPath = path.resolve(__dirname, '../../tools/scripts')
 
-module.exports = robot => {
-  robot.hear("I like pie", (res) => {
-    const exec = require('child_process').exec;
-    const scriptPath = path.resolve(__dirname, '../../tools/scripts')
-    exec(`chmod +x ${scriptPath}/mkdir.sh`, (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-      } else if (stdout) {
-        console.log(stdout);
-      } else {
-        console.log(stderr);
-      }
+module.exports = function(robot) {
+  robot.hear((/^check_migration_changes_\d{8}$/i), function(res) {
+    const fileName = res.match[0];
+    const number = fileName.slice(24);
+    res.send(number)
+    exec = spawn('bash', [scriptPath + '/' + number, 33]);
+    exec.stdout.on('data', function(data) {
+      res.send('データが返ってきた');
+      res.send(data);
     });
-    exec(`${scriptPath}/mkdir.sh`, (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-      } else if (stdout) {
-        console.log(stdout);
-      } else {
-        console.log(stderr);
-      }
-    });
-    res.reply("makes a freshly baked apple pie ver2")
+    exec.stderr.on('data', function(data) {
+      res.send('エラーが返ってきた');
+      res.send(data);
+    })
+  })
+  robot.hear((/^bot_(start|end)$/), function(res) {
+    res.send(res.match[1])
   })
 }
     
--- tools/scripts/mkdir.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p tmp
-touch tmp/first.txt
